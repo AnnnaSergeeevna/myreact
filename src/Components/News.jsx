fe3eb93ea47d3303ea50051a3f0a1f399c5ac000
Aa9214057525 <anna.s.lipatova@gmail.com>
2023-09-25 18:11:23 +0200

    News added local time

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from "react"
+import classes from '../../src/App.css';
 
-const News = () => {
-    return (
-<div>
-    <h2>News</h2>
-    </div>
-    )
+function News() {
+    const [date, setDate] = useState(new Date())
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    }, [])
+    return <>
+        <h2>News</h2>
+        <div className="time">{date.toLocaleTimeString()}</div>
+    </>
+
 }
 export default News;
