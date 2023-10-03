75c603d7e071585eb9b524f09e5cecb803387552
Aa9214057525 <anna.s.lipatova@gmail.com>
2023-10-03 16:25:42 +0200

    change header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,10 +5,10 @@ import classes from './Header.module.css';
 const Header = (props) => {
     return (
         <div className={classes.head}>
-            <div><img src='../headerLogo.png' /> Social Network </div>
+            <div><img src='../headerLogo.png' /> My React App </div>
             <div>
                 <button className={classes.login}>{props.isAuth ? props.login
-                        : <NavLink to={'/login'}>Login</NavLink>}
+                    : <NavLink to={'/login'}>Login</NavLink>}
                 </button>
             </div>
         </div>
